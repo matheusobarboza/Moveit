bf135670c9850954a3312252ada46d131fad428c
matheusobarboza <matheus@paggue.io>
2021-02-25 01:57:31 -0300

    Add styles

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return (
+    <div>
+      <img src="github.com/matheusobarboza.png" alt="Matheus Barboza" />
+      <div>
+          <strong>Matheus Barboza</strong>
+          <p>Level 1</p>
+      </div>
+    </div>
+  );
+}

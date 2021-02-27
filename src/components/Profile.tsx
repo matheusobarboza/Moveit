a30d3dbd1a0be4d5660b79b3f552c57c184e3080
Matheus Barboza <matheus.barboza102@gmail.com>
2021-02-26 21:24:16 -0300

    Teste2

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,7 +3,7 @@ export function Profile() {
     <div>
       <img src="github.com/matheusobarboza.png" alt="Matheus Barboza" />
       <div>
-          <strong>Matheus</strong>
+          <strong>Matheus Barboza</strong>
           <p>Level 1</p>
       </div>
     </div>

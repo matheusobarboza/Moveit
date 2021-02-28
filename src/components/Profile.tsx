826ab548ff11d8d213aa79bca6b32d601f4156dc
Matheus Barboza <matheus.barboza102@gmail.com>
2021-02-28 19:42:20 -0300

    Add component profile and CompletedChallenge

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,10 +1,14 @@
+import styles from '../styles/components/Profile.module.css';
+
 export function Profile() {
   return (
-    <div>
-      <img src="github.com/matheusobarboza.png" alt="Matheus Barboza" />
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/matheusobarboza.png" alt="Matheus Barboza" />
       <div>
           <strong>Matheus Barboza</strong>
-          <p>Level 1</p>
+          <p>
+            <img src="icons/level.svg" alt="level" />
+            Level 1</p>
       </div>
     </div>
   );
